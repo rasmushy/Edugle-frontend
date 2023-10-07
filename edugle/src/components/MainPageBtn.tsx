da5f85d858eea0f1c628cf58ef8f7f9003b7c982
Cl0wnFiesta <clownfiesta98@gmail.com>
2023-10-07 20:54:26 +0300

    s

--- edugle/src/components/MainPageBtn.tsx
+++ edugle/src/components/MainPageBtn.tsx
@@ -1,11 +1,13 @@
-const MainPageBtn = ({handleGoChat, togglePopup}: any) => {
+import Link from "next/link";
+
+const MainPageBtn = ({ handleGoChat, togglePopup }: any) => {
   return (
     <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-      <div onClick={() => handleGoChat()}>
+      <Link href="/chat">
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
           Edu<span className="text-[hsl(280,100%,70%)]">gle</span>
         </h1>
-      </div>
+      </Link>
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
         <button
           className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
