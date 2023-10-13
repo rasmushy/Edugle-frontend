981d09ea49a5c5fa7154979dcbd25367f1051467
Rasmus Hyyppä <rasmus.hyyppa@metropolia.fi>
2023-10-13 14:51:20 +0300

    skipvalidation

--- edugle/src/env.mjs
+++ edugle/src/env.mjs
@@ -9,10 +9,16 @@ export const env = createEnv({
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
     NEXTAUTH_SECRET:
-      process.env.NODE_ENV === "development"
+      process.env.NODE_ENV === "production"
         ? z.string().min(1)
         : z.string().min(1).optional(),
-    NEXTAUTH_URL: z.string().min(1), 
+    NEXTAUTH_URL: z.preprocess(
+      // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
+      // Since NextAuth.js automatically uses the VERCEL_URL if present.
+      (str) => process.env.VERCEL_URL ?? str,
+      // VERCEL_URL doesn't include `https` so it cant be validated as a URL
+      process.env.VERCEL ? z.string().min(1) : z.string(),
+    ),
   },
 
   /**
@@ -33,7 +39,7 @@ export const env = createEnv({
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: "https://edugle.azurewebsites.net/",
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL || "https://edugle.azurewebsites.net/",
     NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL,
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
